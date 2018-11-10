72bf82096f564fd3321207f262820ddf428f5f60
Estudiante <estudiante@iMac3s-iMac.local>
2018-11-10 12:21:00 -0400

    Se agreto el evento para abrir el Servicio

--- app/src/main/java/com/example/estudiante/cda22/ServiceActiviti.kt
+++ app/src/main/java/com/example/estudiante/cda22/ServiceActiviti.kt
@@ -1,12 +1,18 @@
 package com.example.estudiante.cda22
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ServiceActiviti : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_activiti)
+        btnServicios.setOnClickListener {
+            val intent: Intent = Intent(this, ServiceActiviti::class.java)
+            startActivity(intent)
+        }
     }
 }
